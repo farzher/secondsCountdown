8c20131ea70b5caaafecafecfb84e8aac362e4f6
Stephen <stephenkamenar@gmail.com>
2012-11-17 22:34:50 -0500

    init

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(function() {
 
       //explode timer
       var $explode = $this.closest('.explode');
-      if($explode) {
+      if($explode.length) {
         $explode.animate({fontSize:'200px'}, 1000, function() {
           $explode.slideUp('fast');
         });
@@ -14,7 +14,7 @@ $(function() {
 
       //redirect timer
       var $a = $this.siblings('a');
-      if($a) {
+      if($a.length) {
         window.location = $a.attr('href');
         return;
       }
